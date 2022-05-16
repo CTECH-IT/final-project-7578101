023759e0ab3c1e26da96a6ca1f6656985ec2460d
kyler <7578101@isd535.org>
2022-05-16 11:17:26 -0500

    platforms

--- scripts/main.js
+++ scripts/main.js
@@ -7,6 +7,10 @@ let y = 40;
 let playerWidth = 20;
 let playerHeight = 20;
 let dy = 2;
+let touch = 0;
+let platformHeight = 10;
+const platform = [0, canvas.height-platformHeight, canvas.width, 400, 500, 100, 0, 400, 100];
+let numPlatform = 3;
 
 function drawPlayer(){
     ctx.beginPath();
@@ -16,10 +20,35 @@ function drawPlayer(){
     ctx.closePath();
 }
 
+function drawPlatform(){
+    for (let i = 0; i < numPlatform*3; i+=3){
+        ctx.beginPath();
+        ctx.rect(platform[i], platform[i+1], platform[i+2], 10);
+        ctx.fillStyle = "#3BAC2D";
+        ctx.fill();
+        ctx.closePath();
+    }
+}
+
+function platformCollision(){
+    for (let i = 0; i <= numPlatform*3; i+=3){
+        if ((platform[i]-playerWidth<=x && platform[i]+platform[i+2]>=x) && (y+playerHeight>=platform[i+1] && y+playerHeight<=platform[i+1]+platformHeight)){
+            touch=1;
+        }
+
+    }
+}
+
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawPlatform();
     drawPlayer();
-    y+=dy;
+    platformCollision();
+    if (touch==0){
+        y+=dy;
+    }
+    touch=0;
+    
 }
 setInterval(draw, 10);
 
