1317944a809aba415acbd1b91bd07ffd2b7b9cfa
BuildTools <unconfigured@null.spigotmc.org>
2022-05-17 17:14:21 -0500

    finished walls

--- scripts/main.js
+++ scripts/main.js
@@ -20,6 +20,7 @@ let dx = 0;
 let jump = 1;
 let jumpStart = 0.02;
 let jumpSlow = 0.001
+let jumpVelocity = 5;
 let maxSpeed = 6;
 let maxSpeedX = 4;
 let speed = .1;
@@ -28,7 +29,7 @@ let g = 0.1;
 let touch = 0;
 let platformHeight = 10;
 const platformLevel = [[0, canvas.height-platformHeight, canvas.width, 400, 457, 100, 0, 500, 100], [0, canvas.height-platformHeight, canvas.width, 400, 300, 100, 100, 500, 100]];
-const wall = [[100, 100, 100, 100], [50, 400, 100, 100]];
+const wall = [[100, 100, 100, 100], [250, 400, 50, 100, 500, 300, 50, 300]];
 
 
 
@@ -55,7 +56,7 @@ function playerMovement(){
         dx += friction;
     }
     if (spacebar == true && canJump > 0 && jump > 0 && canJump2 == 1){
-        dy = -5;
+        dy = -1*jumpVelocity;
         canJump2 = 0;
         
         //dy -= jump;
@@ -103,14 +104,40 @@ function platformCollision(){
 function screenCollision(){
     if (x<=0 && dx<0){
         x=0;
-        dx=dx/-2
+        dx=dx/-2;
     }
     if (x+playerWidth>=canvas.width && dx>0){
         x=canvas.width-playerWidth;
-        dx=dx/-2
+        dx=dx/-2;
     }
 }
 
+
+function wallCollision(){
+    for (let i = 0; i <= wall[level].length; i+=4){
+        if ((wall[level][i]-playerWidth<=x && wall[level][i]+wall[level][i+2]>=x) && (y+playerHeight>=wall[level][i+1] && y+playerHeight<=wall[level][i+1]+maxSpeed) && dy>=0){
+            dy=0;
+            y=wall[level][i+1]-playerHeight;
+            if (spacebar == false){
+              canJump2 = 1;
+            }
+            canJump = 1;
+        }
+        if ((wall[level][i]-playerWidth<=x && wall[level][i]+wall[level][i+2]>=x) && (y<=wall[level][i+1]+wall[level][i+3] && y>=wall[level][i+1]+wall[level][i+3]-jumpVelocity) && dy<=0){
+            dy=dy/-2;
+            y=wall[level][i+1]+wall[level][i+3];
+            
+        }
+        if ((wall[level][i]-playerWidth<=x && wall[level][i]+maxSpeedX>=x+playerWidth) && (y+playerHeight>wall[level][i+1]+maxSpeed && y<wall[level][i+1]+wall[level][i+3]-jumpVelocity) && dx>=0){
+            dx=dx/-2;
+            x=wall[level][i]-playerWidth;
+        }
+        if ((wall[level][i]+wall[level][i+2]>=x && wall[level][i]+wall[level][i+2]-maxSpeedX<=x) && (y+playerHeight>wall[level][i+1]+maxSpeed && y<wall[level][i+1]+wall[level][i+3]-jumpVelocity) && dx<=0){
+            dx=dx/-2;
+            x=wall[level][i]+wall[level][i+2];
+        }
+    }
+}
 
 
 function draw() {
@@ -118,6 +145,7 @@ function draw() {
     drawPlatform();
     drawWall();
     platformCollision();
+    wallCollision();
     playerMovement();
     canJump = 0;
     screenCollision();
