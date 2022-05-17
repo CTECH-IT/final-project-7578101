49b987f9af048fdbd1d06595110d3f72486b2c8f
kyler <7578101@isd535.org>
2022-05-17 11:28:41 -0500

    added controls

--- scripts/main.js
+++ scripts/main.js
@@ -2,17 +2,33 @@
 let canvas = document.getElementById("myCanvas");
 let ctx = canvas.getContext("2d");
 
+let rightPressed = false;
+let leftPressed = false;
+let spacebar = false;
+let down = false;
+let canJump = 1;
+
+
 let x = 20;
-let y = 40;
+let y = 500;
 let playerWidth = 20;
 let playerHeight = 20;
-let dy = 0;
-let maxSpeed = 3;
-let g = 0.05;
+let dy = -6;
+let dx = 0;
+let jump = 1;
+let jumpStart = 0.02;
+let jumpSlow = 0.001
+let maxSpeed = 4;
+let maxSpeedX = 4;
+let speed = .1;
+let friction = .05;
+let g = 0.1;
 let touch = 0;
 let platformHeight = 10;
 const platform = [0, canvas.height-platformHeight, canvas.width, 400, 500, 100, 0, 400, 100];
 let numPlatform = 3;
+
+
 
 function drawPlayer(){
     ctx.beginPath();
@@ -20,6 +36,30 @@ function drawPlayer(){
     ctx.fillStyle = "#FF0000";
     ctx.fill();
     ctx.closePath();
+}
+
+function playerMovement(){
+    if (rightPressed == true && maxSpeedX >= dx){
+        dx += speed;
+    }
+    if (rightPressed == false && 0 < dx){
+        dx -= friction;
+    }
+    if (leftPressed == true && maxSpeedX*-1 <= dx){
+        dx -= speed;
+    }
+    if (leftPressed == false && 0 > dx){
+        dx += friction;
+    }
+    if (spacebar == true && canJump > 0 && jump > 0){
+        dy = -5;
+        canJump -= 1;
+        //dy -= jump;
+        //jump -= jumpSlow;
+    }
+    if ((-1*friction <= dx && dx <= friction) && rightPressed == false && leftPressed == false){
+        dx = 0;
+    }
 }
 
 function drawPlatform(){
@@ -34,25 +74,84 @@ function drawPlatform(){
 
 function platformCollision(){
     for (let i = 0; i <= numPlatform*3; i+=3){
-        if ((platform[i]-playerWidth<=x && platform[i]+platform[i+2]>=x) && (y+playerHeight>=platform[i+1] && y+playerHeight<=platform[i+1]+platformHeight)){
+        if ((platform[i]-playerWidth<=x && platform[i]+platform[i+2]>=x) && (y+playerHeight>=platform[i+1] && y+playerHeight<=platform[i+1]+platformHeight) && dy>=0){
             dy=0;
+            y=platform[i+1]-playerHeight;
+            canJump = 1;
         }
 
     }
 }
 
+function screenCollision(){
+    if (x<=0 && dx<0){
+        x=0;
+        dx=dx/-2
+    }
+    if (x+playerWidth>=canvas.width && dx>0){
+        x=canvas.width-playerWidth;
+        dx=dx/-2
+    }
+}
+
+
+
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawPlatform();
-    drawPlayer();
     platformCollision();
+    playerMovement();
+    screenCollision();
+    drawPlayer();
     y+=dy;
+    x+=dx;
     if (dy <= maxSpeed){
         dy+=g;
     }
     
     
 }
+
+
+
+
+function keyDownHandler(e) {//detects when keys are pushed down
+    if(e.key=="Right" || e.key == "ArrowRight") {
+        rightPressed = true;
+    }
+    else if(e.key == "left" || e.key == "ArrowLeft") {
+        leftPressed = true;
+    }
+    if(e.key=="ArrowUp" || e.code == "Space") {
+        spacebar = true;
+    }
+    if(e.key=="t") {
+        down = true;
+    }
+
+}
+
+function keyUpHandler(e) {//detects when keys stop being pushed
+    if(e.key=="Right" || e.key == "ArrowRight") {
+        rightPressed = false;
+    }
+    else if(e.key == "Left" || e.key == "ArrowLeft") {
+        leftPressed = false;
+    }
+    if(e.key=="ArrowUp" || e.code == "Space") {
+        spacebar = false;
+    }
+    if(e.key=="t") {
+        down = false;
+    }
+}
+
+
+
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
 setInterval(draw, 10);
 
 
